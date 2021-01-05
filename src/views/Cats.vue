8e05be1723c7e2b2a20cbf7c7ecd4315f4f5ef4b
rashid <rashidvaliyapara@gmail.com>
2021-01-05 18:24:34 +0530

    commit_before_add_pet

---
+++ src/views/Cats.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Available Cats</h1>
+        <b-table striped hover :items="cats">
+            <template #cell(name)="data">
+                <!-- `data.value` is the value after formatted by the Formatter -->
+                <router-link :to="`/pets/cats/${data.index}`">
+                    {{ data.value }}
+                </router-link>
+            </template>
+        </b-table>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState([
+      'cats'
+    ])
+  }
+}
+</script>
